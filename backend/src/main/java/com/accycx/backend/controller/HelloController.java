972748ef3c7397e2501765ff53ed6e13853bfd09
AccyCx <2544966919@qq.com>
2025-12-04 02:02:20 +0800

    Initial commit

---
+++ backend/src/main/java/com/accycx/backend/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.accycx.backend.controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "测试模块", description = "用于测试环境是否跑通")
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+
+    @Operation(summary = "打招呼接口", description = "返回 Hello World")
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Campus Market!";
+    }
+}
